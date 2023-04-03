be0c9b03f7bbcbc75534e37b4e5afdf600efd0ed
Collin Kramp <collin.kramp@protonmail.com>
2023-04-03 03:23:24 +0200

    Removed temporary files, that are no longer needed

--- InformatikQ2/main.js
+++
@@ -1,9 +0,0 @@
-//Eventlistner wartet auf laden des Fensters
-window.addEventListener("load", showDate);
-//Uhr
-function showDate(){
-    let Zeit = new Date();
-    let display = document.getElementById("clock");
-    display.innerText = Zeit.toLocaleTimeString();
-    setInterval(showDate, 1000)
-}
